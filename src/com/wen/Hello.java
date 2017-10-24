84623df60fa835e91582a51291a596367dfdd029
yuwen1227 <coco4522@gmail.com>
2017-10-24 16:34:49 +0800

    算術運算

--- src/com/wen/Hello.java
+++ src/com/wen/Hello.java
@@ -1,12 +1,24 @@
 package com.wen;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 System.out.println("Hello  World");
 Person p = new Person ();
 p.sayHello();
+
+System.out.println("請輸入您的名字:");
+Scanner scanner =new Scanner(System.in);
+String line = scanner.nextLine();
+System.out.println("Hello,"+line);
+
+System.out.println("請問您的年齡?");
+line = scanner.nextLine();
+int age = Integer.parseInt(line);
+System.out.println(age);
+System.out.println(age>=18);
 	}
 
 }
--- src/com/wen/VariableTester.java
+++ src/com/wen/VariableTester.java
@@ -14,13 +14,41 @@ boolean pass = false;
 System.out.println(!pass);
 System.out.println(pass);
 
-String s = new String("Hello");
+
 Random r = new Random();
 int n1 = r.nextInt(6)+1;
 int n2 = r.nextInt(6)+1;
 int n3 = r.nextInt(6)+1;
 int n4 = r.nextInt(6)+1;
 System.out.println( 3>=2 );
+
+String s = new String("Hello");
+int len = s.length();
+System.out.println("len:"+ len);
+System.out.println(s.charAt(0));
+
+//+-*/ %餘數    &&且    ||或
+float f =10.0f;
+int x =10;
+int y =3;
+float z =f/y;
+System.out.println(z);
+
+//2進位運算 10=01010
+System.out.println(x>>1);
+
+//x++; => x=x+1;
+x++;
+x--;
+System.out.println(x);
+
+//x=x+2; =>x += 2;
+x = x+2;
+x += 2;
+System.out.println(x);
+
+
+
 	}
 
 }
