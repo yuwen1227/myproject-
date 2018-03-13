4cac019275046bc214446218f2a7135f2206a0b5
yuwen1227 <coco4522@gmail.com>
2018-03-13 16:32:10 +0800

    練習

---
+++ src/com/java2/object/SilverCustomer.java
@@ -0,0 +1,10 @@
+package com.java2.object;
+
+public class SilverCustomer extends Customer{
+  public SilverCustomer(int amount){
+	  super(amount);
+	  float discount = 0.1f;
+  }
+	
+	
+}
---
+++ src/com/java2/object/Ticket.java
@@ -0,0 +1,5 @@
+package com.java2.object;
+
+public class Ticket {
+ // 站台Station 號id 名name 
+}
