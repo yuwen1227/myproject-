0aff9d322ef5ea7b0a4508a102015c10a42195c3
yuwen1227 <coco4522@gmail.com>
2018-05-08 16:27:35 +0800

    set tester2

--- src/com/java2/collection/Main.java
+++ src/com/java2/collection/Main.java
@@ -4,25 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//Tree set  不重複 依大小排序
+//Hash set  不重複 不照順序  is faster than Tree set
+//ArrayList
+//LinkedList
+
 public class Main {
-
-
-	List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
-
-
-	Object[] strArray = nameList.toArray();
-
-
-    for (int i = 0; i < strArray.length; i++) {
-      System.out.println(strArray[i]);
-    }
-
-  }
+	  public static void main(String[] args) {
+		  List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+		  list.remove(0);
+		  list.remove(1);
+		  list.remove(2);
+		  list.remove(3);
+		  System.out.println(list);
 }
-   
-
-
-
-
-
-	  
+} 
---
+++ src/com/java2/collection/SetTester.java
@@ -0,0 +1,17 @@
+package com.java2.collection;
+
+
+public class SetTester {
+
+/*  6
+	postgres
+	sqlite
+	oracle
+	mongodb
+	postgres
+	mssql
+*/
+	public static void main(String[] args) {
+		 Set<String> set = new TreeSet<>(Arrays.asList("6","postgres","sqlite","oracle","mongodb","mongodb","postgres","mssql"));
+	}
+	}
