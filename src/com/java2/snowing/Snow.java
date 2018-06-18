8441b93c39b4bf9024302191019e00fb306b798d
yuwen1227 <coco4522@gmail.com>
2018-06-18 18:44:24 +0800

    雪花 開新視窗

--- src/com/java2/snowing/Snow.java
+++ src/com/java2/snowing/Snow.java
@@ -2,24 +2,40 @@ package com.java2.snowing;
 
 import java.util.Random;
 
+import com.java2.snowing.SnowFrame.SnowCanvas;
+
 public class Snow extends Thread{
 	int x;
 	int y = 800;
 	Random random = new Random();
-	public Snow(){
-		x = random.nextInt(600);
+	private final SnowFrame.SnowCanvas snowCanvas;
+	
+	
+	public Snow(SnowFrame.SnowCanvas snowCanvas) {
+		this.snowCanvas = snowCanvas;
+		x = random.nextInt(800);
+		y = random.nextInt(600);
 	}
+	
+	
+	
+	
+	
 	@Override
 	public void run(){
 		for(int i = 800; i > 0; i--){
 //			System.out.println(getName()+":("+ x +","+ y +")");
-			y =  y - 1;
-			int movement = random.nextInt(3)-1;
-			x = x-movement; 
-			
+			y++;
+			x = x + random.nextInt(3)-1;
+			snowCanvas.repaint();
+			try {
+				sleep(50);
+			} catch ( InterruptedException e) {
+				e.printStackTrace();
+			}
 			//前面空格
-			System.out.print(new String(new char[x]).replace("\0"," "));
-			System.out.println("雪");
+			//System.out.print(new String(new char[x]).replace("\0"," "));
+			//System.out.println("雪");
 	}
 	
 	
--- src/com/java2/snowing/SnowFrame.java
+++ src/com/java2/snowing/SnowFrame.java
@@ -8,26 +8,40 @@ import java.util.List;
 import javax.swing.JFrame;
 
 public class SnowFrame extends JFrame {
-	public class SnowCanvas extends Canvas{
+	SnowCanvas canvas = new SnowCanvas();
+
+	public SnowFrame() {
+		setSize(850, 650);
+		setLocation(500, 200);
+		add(canvas);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public class SnowCanvas extends Canvas {
 		List<Snow> snows = new ArrayList<>();
+
 		public SnowCanvas() {
-			for(int i =0 ; i<100; i++) {
-				Snow snow = new Snow();
-				snow.start();
+			for (int i = 0; i < 100; i++) {
+				Snow snow = new Snow(SnowCanvas.this);
 				snows.add(snow);
-				
+				snow.start();
+
 			}
 		}
+
 		
+
+		@Override
+		public void paint(Graphics g) {
+			for (int i = 0; i < 100; i++) {
+				Snow snow = snows.get(i);
+				g.drawString("雪", snow.x, snow.y);
+			}
+		}
+
 	}
 	public static void main(String[] args) {
-		
+		new SnowFrame();
 	}
-	public void paint(Graphics g) {
-		for(int i =0;i<100;i++) {
-			Snow snow = snows.get(i);
-			g.drawString("雪", snow.x, snow.y);
-		}
-	}
-		
-	}
+}
