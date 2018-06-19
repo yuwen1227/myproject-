2fadd2da4f4c1569987010cdaa8a9469a2ed37a0
yuwen1227 <coco4522@gmail.com>
2018-06-19 15:31:30 +0800

    分支筆記

--- src/com/java2/net/ChatServer.java
+++ src/com/java2/net/ChatServer.java
@@ -1,3 +1,9 @@
+//建立新分支(chat) : git branch chat
+//切換到 branch chat : git checkout chat
+//一起git push : git push --all origin 
+//合併分支 : git merge chat (先git push再合併)
+//儲存離開 : :wq 
+
 package com.java2.net;
 
 public class ChatServer {
