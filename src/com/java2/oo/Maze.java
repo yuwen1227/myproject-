27fdc6f835ea325ee15d3f3249fc5574532f9fce
yuwen1227 <coco4522@gmail.com>
2018-06-22 01:29:33 +0800

    maze(研究中)

--- src/com/java2/oo/Maze.java
+++ src/com/java2/oo/Maze.java
@@ -3,5 +3,12 @@ package com.java2.oo;
 public class Maze {
 	int row;
 	int col;
-	int
+	int trapCount();
+	int[] Location;
+	Player player = new Player();
+	}
+	 	public Maze(int row,int col,int trapCount) {
+	 		this.row = row;
+	 		this.col = col;
+	 		this.trapCount = trapCount;
 }
