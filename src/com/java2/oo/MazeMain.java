bec5873e5f3b903e8714b7588ca37f44c77f995c
yuwen1227 <coco4522@gmail.com>
2018-06-23 22:12:25 +0800

    Maze(參考 還不會)

--- src/com/java2/oo/MazeMain.java
+++ src/com/java2/oo/MazeMain.java
@@ -24,9 +24,147 @@ public class MazeMain {
 			int col = Integer.parseInt(steps[0]);
 			int row = Integer.parseInt(steps[1]);
 			line = bfr.readLine();
-		
-		} catch (Exception e) {
-		} 
+			int snareCount = Integer.parseInt(line);
+			Maze a = new Maze(col, row, snareCount);
+			Random r = new Random();
+			Set<String> set = new TreeSet<>();
+			while (set.size() < 5) {
+				String snare = String.valueOf(r.nextInt(24));
+				set.add(snare);
+			}
+			Iterator it = set.iterator();
+			a.Location = new int[set.size()];
+			for (int b = 0; b < set.size(); b++) {
+				int i = Integer.parseInt(String.valueOf(it.next()));
+				a.Location[b] = i;
+				System.out.println(a.Location[b]);
+			}
+			int walk;
+			line = bfr.readLine();
+			String step[] = line.split(",");
+			Scanner scanner = new Scanner(System.in);
+			for (int i = 0; i < steps.length; i++) {
+				if (a.player.HP > 0) {
+					walk = 0;
+					System.out.println("請輸入8(上)2(下)4(左)6(右)：");
+					System.out.println(steps[i]);
+					switch (steps[i]) {
+					case "8":
+						if (a.player.location % 6 == 0) {
+							System.out.println("撞牆!");
+							a.player.HP -= 5;
+						} else {
+							a.player.location -= 6;
+							for (int b = 0; b < set.size(); b++) {
+								if (a.Location[b] == a.player.location) {
+									walk = 1;
+								}
+							}
+							if (walk == 1) {
+								a.player.HP -= 20;
+							} else {
+								a.player.HP -= 1;
+							}
+						}
+						if (a.player.HP > 0) {
+							System.out.println("位置：" + a.player.location);
+							System.out.println("血量：" + a.player.HP);
+							break;
+						} else {
+							System.out.println("Game Over!");
+							break;
+						}
+					case "2":
+						if (a.player.location % 6 == 3) {
+							System.out.println("撞牆!");
+							a.player.HP -= 5;
+						} else {
+							a.player.location += 6;
+							for (int b = 0; b < set.size(); b++) {
+								if (a.Location[b] == a.player.location) {
+									walk = 1;
+								}
+							}
+							if (walk == 1) {
+								a.player.HP = 20;
+							} else {
+								a.player.HP = 1;
+							}
+						}
+						if (a.player.HP > 0) {
+							System.out.println("位置：" + a.player.location);
+							System.out.println("血量：" + a.player.HP);
+							break;
+						} else {
+							System.out.println("Game Over!");
+							break;
+						}
+					case "4":
+						if (a.player.location % 6 == 0) {
+							System.out.println("撞牆!");
+							a.player.HP -= 5;
+						} else {
+							a.player.location -= 1;
+							for (int b = 0; b < set.size(); b++) {
+								if (a.Location[b] == a.player.location) {
+									walk = 1;
+								}
+							}
+							if (walk == 1) {
+								a.player.HP -= 20;
+							} else {
+								a.player.HP -= 1;
+							}
+						}
+						if (a.player.HP > 0) {
+							System.out.println("目前位置：" + a.player.location);
+							System.out.println("目前血量：" + a.player.HP);
+							break;
+						} else {
+							System.out.println("Game Over!");
+							break;
+						}
+					case "6":
+						if (a.player.location % 6 == 5) {
+							System.out.println("撞牆!");
+							a.player.HP -= 5;
+						} else {
+							a.player.location += 1;
+							for (int b = 0; b < set.size(); b++) {
+								if (a.Location[b] == a.player.location) {
+									walk = 1;
+								}
+							}
+							if (walk == 1) {
+								a.player.HP -= 20;
+							} else {
+								a.player.HP -= 1;
+							}
+						}
+						if (a.player.HP > 0) {
+							System.out.println("位置：" + a.player.location);
+							System.out.println("血量：" + a.player.HP);
+							break;
+						} else {
+							System.out.println("Game Over!");
+							break;
+						}
+					default:
+						break;
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new MazeMain();
+
 	}
 }
-			
+		
