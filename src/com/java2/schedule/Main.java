6a480389a2c85c3b6496e46d9eda0f407ed4fbf4
yuwen1227 <coco4522@gmail.com>
2018-06-26 11:44:20 +0800

    schedule完成

--- src/com/java2/schedule/Main.java
+++ src/com/java2/schedule/Main.java
@@ -1,7 +1,9 @@
 package com.java2.schedule;
 
 import java.util.List;
+import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,27 +19,123 @@ import java.util.ArrayList;
  * [有課:C004 English 到16時]
  */
 public class Main {
-	List<Course> courses = new ArrayList<>();
-			
-	public Main(){
-		readCourses();
-	}
-	public void readCourses(){
+	Scanner scanner = new Scanner(System.in);
+
+	public void study() {
+			int function = 0;
+			while(function!=2) {
+				showFuctions();
+				function = scanner.nextInt();
+			switch(function) {
+			case 1:
+				System.out.println("請輸入星期(1~7):");
+				int day = scanner.nextInt();
+				System.out.println("請輸入時(0~24):");
+				int time = scanner.nextInt();
+				
 		try {
-			BufferedReader in = new BufferedReader(new FileReader("schedule.txt"));
-		} catch (IOException e) {
-			// TODO: handle exception
-		}
-	}
-	public static void main(String[] args) {
-		FileReader fr;
-		try {
-			fr = new FileReader("schdule.txt");
+			FileReader fr = new FileReader("schedule.txt");
 			BufferedReader in = new BufferedReader(fr);
 			String line = in.readLine();
-			String 
+				String[] token1 = line.split(",");
+				List<String> list1 = new ArrayList<>();
+				for(int i = 0;i<token1.length;i++) {
+					list1.add(token1[i]);
+				}
+				line = in.readLine();
+				String[] token2 = line.split(",");
+				List<String> list2 = new ArrayList<>();
+				for(int i = 0;i<token2.length;i++) {
+					list2.add(token2[i]);
+				}
+				line = in.readLine();
+				String[] token3 = line.split(",");
+				List<String> list3 = new ArrayList<>();
+				for(int i = 0;i<token3.length;i++) {
+					list3.add(token3[i]);
+				}
+				line = in.readLine();
+				String[] token4 = line.split(",");
+				List<String> list4 = new ArrayList<>();
+				for(int i = 0;i<token4.length;i++) {
+					list4.add(token4[i]);
+				}
+				
+				
+				
+				
+
+				switch(day) {
+				case 1:
+					if(time>=Integer.parseInt(list3.get(3))&&time<=(Integer.parseInt(list3.get(3))+Integer.parseInt(list3.get(4)))) {
+						System.out.println("[有課:" + list3.get(0) + list3.get(1) + "到"+(Integer.parseInt(list3.get(3))+Integer.parseInt(list3.get(4)))+"時]");
+						break;
+					}else {
+						System.out.println("[有空檔]");
+						break;
+					}
+					
+				case 4:
+				
+					
+					if(time>=Integer.parseInt(list2.get(3))&&time <=(Integer.parseInt(list2.get(3))+Integer.parseInt(list2.get(4)))){
+							System.out.println("[有課:" + list2.get(0) + list2.get(1) + "到"+(Integer.parseInt(list2.get(3))+Integer.parseInt(list2.get(4)))+"時]");
+							break;
+					}
+					else {
+						if(time>=Integer.parseInt(list4.get(3))&&time <=(Integer.parseInt(list4.get(3))+Integer.parseInt(list4.get(4)))){
+							System.out.println("[有課:" + list4.get(0) + list4.get(1) + "到"+(Integer.parseInt(list4.get(3))+Integer.parseInt(list4.get(4)))+"時]");
+							break;
+					}else {
+						System.out.println("[有空檔]");
+						break;
+					}
+					}
+					
+					
+				
+				case 2:
+					if(time>=Integer.parseInt(list1.get(3))&&time <=(Integer.parseInt(list1.get(3))+Integer.parseInt(list1.get(4)))){
+						System.out.println("[有課:" + list1.get(0) + list1.get(1) + "到"+(Integer.parseInt(list1.get(3))+Integer.parseInt(list1.get(4)))+"時]");
+						break;
+				}else {
+					System.out.println("[有空檔]");
+					break;
+				}
+					default:
+						System.out.println("[有空檔]");
+						break;
+				}
+				
+			}
+		 catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+			case 2:
+				return;
+		}
+		}
+		}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.study();
+
+	}
+
+	public void showFuctions() {
+		System.out.println("請輸入功能(1or2)");
+		System.out.println("1)查詢空檔時間");
+		System.out.println("2)結束程式");
+	}
+
+	public void question() {
+		System.out.println("請輸入星期(1~7):");
+		int day = scanner.nextInt();
+		System.out.println("請輸入時(0~24):");
+		int time = scanner.nextInt();
 	}
 
 }
--- src/com/java2/schedule/Schedule.java
+++ src/com/java2/schedule/Schedule.java
@@ -1,5 +1,38 @@
 package com.java2.schedule;
 
 public class Schedule {
+	
+		String classroom;
+		String name;
+		int day1;
+		int time1;
+		int due1;
+		public Schedule() {
+			
+		}
+		public Schedule(String classroom,String name,int day1,int time1,int due1) {
+			this.classroom = classroom;
+			this.name = name;
+			this.day1 = day1;
+			this.time1 = time1;
+			this.due1 = due1;
+		}
+		public String getclassroom() {
+			return classroom;
+		}
+		public String getname() {
+			return name;
+		}
+		public int getday1() {
+			return day1;
+		}
+		public int gettime1() {
+			return time1;
+		}
+		public int getdue1() {
+			return due1;
+		}
+		
 
-}
+	}
+
